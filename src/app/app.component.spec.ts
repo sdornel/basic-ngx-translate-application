afbfe106461bb7dc1350cd104b13bff7e6c24cc7
sdornel <kai_dornel@hotmail.com>
2024-10-14 11:21:13 -0400

    add unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,77 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { Language } from './enums/language.enum';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { of, Subject } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let translateService: TranslateService;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  const onTranslationChange$ = new Subject();
+  const onLangChange$ = new Subject();
+  const onDefaultLangChange$ = new Subject();
+
+  const mockTranslateService = {
+    setDefaultLang: jasmine.createSpy('setDefaultLang'),
+    use: jasmine.createSpy('use'),
+    get: jasmine.createSpy('get').and.returnValue(of('mocked value')),
+    onTranslationChange: onTranslationChange$.asObservable(),
+    onLangChange: onLangChange$.asObservable(),
+    onDefaultLangChange: onDefaultLangChange$.asObservable(),
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      declarations: [AppComponent],
+      providers: [
+        { provide: TranslateService, useValue: mockTranslateService }
+      ]
+    }).compileComponents();
   });
 
-  it(`should have as title 'basic-ngx-translate-application'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('basic-ngx-translate-application');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    translateService = TestBed.inject(TranslateService);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('basic-ngx-translate-application app is running!');
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set default language to French on init', () => {
+    component.ngOnInit();
+    expect(translateService.setDefaultLang).toHaveBeenCalledWith(Language.French);
+  });
+
+  it('should call translate.use() when switchLanguage is triggered', () => {
+    const mockEvent = {
+      target: {
+        value: Language.Spanish
+      }
+    } as any;
+
+    component.switchLanguage(mockEvent);
+    expect(translateService.use).toHaveBeenCalledWith(Language.Spanish);
+  });
+
+  it('should have French as the default selectedLanguage', () => {
+    expect(mockTranslateService.setDefaultLang).toHaveBeenCalledWith(Language.French);
+  });
+
+  it('should switch to the selected language when an option is selected', () => {
+    const selectElement: DebugElement = fixture.debugElement.query(By.css('select'));
+
+    selectElement.triggerEventHandler('change', { target: { value: Language.Romanian } });
+    fixture.detectChanges();
+
+    expect(mockTranslateService.use).toHaveBeenCalledWith(Language.Romanian);
+    expect(mockTranslateService.use).toHaveBeenCalledWith(Language.Romanian); // Expect 'ro'
   });
 });
