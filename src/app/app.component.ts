07e35844c10e27b2ef79642ab09f9455d7cbfcdc
sdornel <kai_dornel@hotmail.com>
2024-10-14 11:21:05 -0400

    initial landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import { Language } from './enums/language.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'basic-ngx-translate-application';
+export class AppComponent implements OnInit {
+  Language = Language; // make enum accessible in template
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang(Language.French);
+  }
+
+  switchLanguage(event: Event) {
+    const language = (event.target as HTMLSelectElement).value
+    this.translate.use(language);
+  }
 }
